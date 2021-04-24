39ddf0e3755b18128e69e657715b8b54cdc519d8
Armin Arabi <ArminWhoCode@gmail.com>
2021-04-24 15:41:47 +0430

    create inputText state

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 
 //import components
 import Form from "../Components/Form/Form";
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <h1 className="App__header">React Todo List</h1>
