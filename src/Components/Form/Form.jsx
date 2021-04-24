c3be864922cd221a3a2e76b926be7898f3c1dee1
Armin Arabi <ArminWhoCode@gmail.com>
2021-04-24 15:25:08 +0430

    create Form component

---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <form>
+      <input type="text" />
+      <button type="submit">
+        <i className="fa fa-plus-square fa-lg"></i>
+      </button>
+    </form>
+  );
+};
+
+export default Form;
--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.scss";
 
+//import components
+import Form from "../Components/Form/Form";
+
 function App() {
   return (
     <div className="App">
       <h1 className="App__header">React Todo List</h1>
+      <Form />
     </div>
   );
 }
